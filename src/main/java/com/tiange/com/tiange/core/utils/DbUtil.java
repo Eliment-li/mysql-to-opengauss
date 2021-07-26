11d85d88f592266101d42aa35632c64d32d5f512
李天 <49738633+Eliment-li@users.noreply.github.com>
2021-07-26 15:11:35 +0800

    add 数据库多行查询 转换为 beanlist

--- src/main/java/com/tiange/com/tiange/core/utils/DbUtil.java
+++ src/main/java/com/tiange/com/tiange/core/utils/DbUtil.java
@@ -1,9 +1,6 @@
 package com.tiange.com.tiange.core.utils;
 
-import org.apache.commons.dbutils.BasicRowProcessor;
-import org.apache.commons.dbutils.BeanProcessor;
 import org.apache.commons.dbutils.QueryRunner;
-import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
 
 import java.sql.Connection;
@@ -11,14 +8,11 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 
-import java.lang.annotation.ElementType;
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-import java.lang.annotation.Target;
+import   com.tiange.com.tiange.*;
 
 public class DbUtil {
 
-
+    static  Connection connection=null;
     /**
      * 获取数据库连接
      * @param url 数据库地址
@@ -26,14 +20,38 @@ public class DbUtil {
      * @param password 密码
      * @return Connection
      */
-    public static Connection getConnection(String url, String username, String password) {
-        Connection connection=null;
+     static Connection getConnection(String url, String username, String password) {
         try {
             connection = DriverManager.getConnection(url, username, password);
         } catch (Exception e) {
             e.printStackTrace();
         }
         return connection;
+    }
+
+
+    /**
+     * 读取数据库多行记录，转换为对象
+     * @param connection 数据库连接
+     * @param sql    查询语句
+     * @param clazz  对象类型
+     * @return List<?> 结果集
+     */
+     static List<?> queryForBeans(Connection connection,String sql,Class clazz){
+
+        QueryRunner queryRunner = new QueryRunner();
+
+        BeanListHandler<?> beanListHandler = new BeanListHandler(clazz);
+        try {
+            List<?> empList = queryRunner.query(connection, sql, beanListHandler);
+
+            return empList;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
     }
 
     public static void execute(Connection connection, String sql) throws SQLException {
